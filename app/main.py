8fd014587ea1ec3561203a2d16370795df6ba9ea
sudjoao <isudjoao@gmail.com>
2023-02-05 21:05:14 -0300

    feat: create prediction endpoint

--- app/main.py
+++ app/main.py
@@ -11,3 +11,23 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "Access /docs to know more about this api"}
+
+@app.post("/prediction")
+async def calculate_forecast(forecast_body: ForecastBody) -> list[float]:
+    #Recebendo array de 24 valores para 24 horas
+    data = np.array(forecast_body.hour_history)
+
+    #Reshape
+    data = data.reshape(-1,1)
+
+    #Treinando o modelo usando e preparando para extracao de 6 valores
+    model = LinearRegression().fit(data[:-6], data[6:])
+
+    #prevendo
+    prediction = model.predict(data)
+    next_six_hours_prediction = prediction[-6:]
+    result = []
+    for array in next_six_hours_prediction:
+        for item in array:
+            result.append(item)
+    return result
