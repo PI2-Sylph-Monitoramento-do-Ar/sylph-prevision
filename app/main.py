5c6763e11ed517e90d563b3da6fec4006901e0b7
sudjoao <isudjoao@gmail.com>
2023-02-10 07:34:57 -0300

    fix: accept any array and sanitized it

--- app/main.py
+++ app/main.py
@@ -3,10 +3,17 @@ from fastapi import FastAPI
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from pydantic import BaseModel
-
+import numbers
 class ForecastBody(BaseModel):
-    hour_history: list[float]
+    hour_history: list
 app = FastAPI()
+
+def sanitize_array(array):
+    new_array = []
+    for item in array:
+        if(isinstance(item, numbers.Number)):
+            new_array.append(item)
+    return new_array
 
 @app.get("/")
 def read_root():
@@ -14,10 +21,11 @@ def read_root():
 
 @app.post("/prediction")
 async def calculate_forecast(forecast_body: ForecastBody) -> float:
-    data = np.array(forecast_body.hour_history)
+    sanitized_array = sanitize_array(forecast_body.hour_history)
+    print(sanitized_array)
+    data = np.array(sanitized_array)
     data = data.reshape(-1,1)
     model = LinearRegression().fit(data[:-1], data[1:])
     prediction = model.predict(data)
-    print(prediction)
     result = prediction[0][0]
     return result
